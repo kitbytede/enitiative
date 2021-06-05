81789c883b6ea6d1f81d94ba080dae6df8819262
WPC/wk <wladimir@kitbyte.de>
2021-06-05 08:28:04 +0200

    DB get

--- inc/db.class.php
+++ inc/db.class.php
@@ -1,5 +1,27 @@
 <?php
 class DB{
+	private $pdo;
+	function __construct(){
+		global $config;
 
+		$dbstring  = 'mysql:host=' . $config['db']['host'];
+		$dbstring .= ';dbname=' . $config['db']['dbname'];
+		$dbstring .= ';charset=UTF8';
+
+		$this->pdo = new PDO($dbstring, $config['db']['user'], $config['db']['pass']);
+		$this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+
+	}
+
+	public function get_page_data($page_id){
+		$stmt = $this->pdo->prepare("
+			SELECT id, title, content
+			FROM pages
+			WHERE id = ?
+			LIMIT 0,1");
+		$stmt->execute([$page_id]); 
+		$page = $stmt->fetch();
+		return $page;
+	}
 }
 ?>
--- index.php
+++ index.php
@@ -5,4 +5,7 @@ require_once('config.inc.php');
 // DB
 require_once('inc/db.class.php');
 $db = new DB();
+
+$page = $db->get_page_data(1);
+var_dump($page);
 ?>
