9969c70554cc28531ad02a220eae918bfc149bd0
WPC/wk <wladimir@kitbyte.de>
2021-06-05 09:17:36 +0200

    template engine

---
+++ inc/tpl.class.php
@@ -0,0 +1,21 @@
+<?php
+class Tpl{
+	private $html;
+
+	public function __construct($tpl_name){
+		$this->html = file_get_contents('tpl/' . $tpl_name . '.tpl.html');
+	}
+
+	public function set($page_data){
+		if(is_array($page_data)){
+			foreach($page_data as $key=>$value){
+				$this->html = str_replace('[' . strtoupper($key) . ']', $value, $this->html);
+			}
+		}
+	}
+
+	public function __tostring(){
+		return (string)$this->html;
+	}
+}
+?>
--- index.php
+++ index.php
@@ -6,9 +6,27 @@ require_once('config.inc.php');
 require_once('inc/db.class.php');
 $db = new DB();
 
+// Template
+require_once('inc/tpl.class.php');
+
 // $new_page = $db->create_page_data('test', 'bla');
 // $edited_page = $db->update_page_data(2, 'edited', 'bla edited');
 
-$page = $db->get_page_data(1);
-var_dump($page);
+if(empty($_GET['id'])){ // default page
+	$_GET['id'] = 1;
+}
+
+if(empty($_GET['admin'])){ // prepare template
+	$tpl_name = 'default';
+}
+else{
+	$tpl_name = 'admin';
+}
+$tpl = new Tpl($tpl_name);
+
+$page_data = $db->get_page_data($_GET['id']);
+$tpl->set($page_data);
+
+// output
+echo $tpl;
 ?>
