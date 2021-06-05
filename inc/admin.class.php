03135e0167b3e860eb6ccd37799720771d4f13c6
WPC/wk <wladimir@kitbyte.de>
2021-06-05 13:16:09 +0200

    admin style separated, Admin-Navi

--- inc/admin.class.php
+++ inc/admin.class.php
@@ -1,7 +1,21 @@
 <?php
 class Admin{
+	private $page_data;
+
+	public function __construct(){
+		if(isset($_SESSION['admin'])){
+			$this->page_data['btn_logout'] = '
+				<a 	class="btn btn-dark px-3"
+					href="[HOME]admin/?logout"
+					role="button"><i class="bi bi-box-arrow-right"></i> Logout</a>';
+		}
+		else{
+			$this->page_data['btn_logout'] = '';
+		}
+	}
+
 	public function get_page_data(){
-		
+		return $this->page_data;
 	}
 }
 ?>
